a82af5e0e64debda5f6298ac1284ee1d027edb9f
Zaky <zakyabdurrahman10@gmail.com>
2021-12-28 16:07:15 +0700

    refactor the entire calculation mechanism and rounding decimal function

--- calc.js
+++ calc.js
@@ -1,7 +1,7 @@
 const display = document.querySelector('#display');
-let tempOperator;
+let tempOperators = [];
+let numbers = [];
 let tempValue = 0;
-let displayLength;
 let calcStatus = false; //if a calculation is complete, true, else false so user can't prematurely click =
 //number buttons
 const numberButtons = document.querySelectorAll('.number');
@@ -14,19 +14,31 @@ numberButtons.forEach(e => e.addEventListener('click', (ev) => {
     let number = ev.target.textContent;
     (display.textContent === '0') ? display.textContent = number : display.textContent += number; 
     //get the second number without the operator
-    let operatorLoc = display.textContent.match(/[\+÷\-x]+/);
-    if (operatorLoc){
-        let nextNumber = display.textContent.slice(operatorLoc.index + 1);
-        console.log(nextNumber);
-        tempValue = operate(tempOperator, parseFloat(tempValue), parseFloat(nextNumber));
-    };
-    displayLength = display.textContent.length;
+    
+    let operators = [...display.textContent.matchAll(/[\+÷\-x]+/g)];
+    //find the last item
+    let lastItem = operators[operators.length - 1];
+    if (lastItem) {
+        tempOperators.push(lastItem['0']);
+    }
+    console.log(tempOperators)
+    //put all the operators and numbers to their respective arrays
+    
     
 }));
 
 commenceButton.addEventListener('click', ev => {
-    display.textContent = tempValue;
+    let tempNumbers = [...display.textContent.matchAll(/\d+/g)];
+    tempNumbers = tempNumbers.map(e => parseFloat(e['0']));
     
+    display.textContent = tempValue;
+    console.log(tempNumbers);
+    tempValue = 0;
+    //compute using the two array, the limit is on the number so surplus operator is ignored
+    //clean up
+    if(numbers.length !== 0) numbers.splice(0, numbers.length);
+    if(tempOperators.length !== 0) tempOperators.splice(0, tempOperators.length);
+
 })
 
 clearButton.addEventListener('click', ev => {
@@ -37,15 +49,18 @@ clearButton.addEventListener('click', ev => {
 opButton.forEach(e => e.addEventListener('click', (ev) => {
     let operator = ev.target.textContent;
     
-    tempValue = parseFloat(display.textContent);
-    tempOperator = operator;
+    
     
     (display.textContent === '0') ? display.textContent = operator : display.textContent += operator}));
 
 //the fun stuff, add the operator to display, save the value to 0 array
 
 function roundTwo(i){
-    return Math.round(i * 100) / 100;
+    if ((i * 10) % 10 !== 0){
+        return Math.round(i * 100) / 100;
+    } else {
+        return i;
+    }
 }
 
 
@@ -59,8 +74,8 @@ function operate(operator, a, b){
             return a * b;
         case '÷':
             if (b === 0) {
-                alert('DO NOT DIVIDE BY ZERO!');
-                return;
+                
+                return 'no';
             }
             return a / b;
         default:
